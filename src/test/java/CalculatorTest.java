db1a4bcfec7f5e5d191984a62918bd5cd0ca93c0
mxlq1 <max_libov@mail.ru>
2023-04-26 19:20:20 +0300

    added mult and abs and tests

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -10,4 +10,18 @@ public class CalculatorTest {
         int res = calc.subtract(10, 5);
         assertEquals(5, res);
     }
+    @Test
+    public void testmultiplyByZero(){
+        Calculator calc = new Calculator();
+        assertNotNull(calc);
+        int res = calc.multiplyByZero(10);
+        assertEquals(0, res);
+    }
+    @Test
+    public void testabs(){
+        Calculator calc = new Calculator();
+        assertNotNull(calc);
+        int res = calc.abs(-10);
+        assertEquals(-10, res);
+    }
 }
